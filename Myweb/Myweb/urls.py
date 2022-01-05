80d340e7428936c3afabffb08ddc9d8a3c113d3f
Mbaje Rwaka Eric <60686069+MBAJERWAKAERIC@users.noreply.github.com>
2022-01-05 15:40:48 +0300

    Update urls.py

--- Myweb/Myweb/urls.py
+++ Myweb/Myweb/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import Myweb.Myapp.models
+from Myweb.Myapp import urls
+from Myweb.Myapp.models import Myapp
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Myweb.Myapp.models.Myapp/', include('Myapp.urls')),
 ]
