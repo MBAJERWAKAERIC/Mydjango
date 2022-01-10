d642ca06a1c53327b6346679a260a8a8c2146512
Mbaje Rwaka Eric <60686069+MBAJERWAKAERIC@users.noreply.github.com>
2022-01-10 17:37:42 +0300

    Update urls.py

--- Myweb/Myapp/urls.py
+++ Myweb/Myapp/urls.py
@@ -3,3 +3,8 @@ from . import views
 urlpatterns = [
 path('', views.index, name = 'index'),
 ]
+from django.urls import path
+from . import views
+urlpatterns = [
+path('', views.student_show, name = 'student_show'),
+]
