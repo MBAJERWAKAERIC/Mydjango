24191e63138411e987a3b8e812cd63c4a1efdf79
Mbaje Rwaka Eric <60686069+MBAJERWAKAERIC@users.noreply.github.com>
2022-01-10 15:56:56 +0300

    Update models.py

--- Myweb/Myapp/models.py
+++ Myweb/Myapp/models.py
@@ -1,9 +1,13 @@
 from django.db import models
-
-
 # Create your models here.
 class Myapp(models.Model):
     roll_no = models.TextField()
     name = models.TextField(max_length=40)
+    stud_class = models.TextField()
+    department = models.TextField()
+    
+class Student(models.Model):
+    roll_no = models.TextField()
+    name = models.TextField(max_length = 40)
     stud_class = models.TextField()
     department = models.TextField()
