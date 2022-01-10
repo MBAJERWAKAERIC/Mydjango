96ae41e1b0f37579fbb88fac0b7fd80f8f2b0b5a
Mbaje Rwaka Eric <60686069+MBAJERWAKAERIC@users.noreply.github.com>
2022-01-10 17:37:58 +0300

    Update models.py

--- Myweb/Myapp/models.py
+++ Myweb/Myapp/models.py
@@ -5,7 +5,7 @@ class Myapp(models.Model):
     name = models.TextField(max_length=40)
     stud_class = models.TextField()
     department = models.TextField()
-    
+
 class Student(models.Model):
     roll_no = models.TextField()
     name = models.TextField(max_length = 40)
