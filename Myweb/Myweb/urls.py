54475afac70973d61b9968686f89dd7a8781b61d
Mbaje Rwaka Eric <60686069+MBAJERWAKAERIC@users.noreply.github.com>
2022-01-10 15:56:14 +0300

    Update urls.py

--- Myweb/Myweb/urls.py
+++ Myweb/Myweb/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Myweb.Myapp.models.Myapp/', include('Myapp.urls')),
+    path('Myweb/', include('Myapp.urls')),
+    path('student/', include('student.urls')),
 ]
