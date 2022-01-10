733dd4e35151879993139c2213a0fa0c994d81cb
Mbaje Rwaka Eric <60686069+MBAJERWAKAERIC@users.noreply.github.com>
2022-01-10 15:56:29 +0300

    Update views.py

--- Myweb/Myapp/views.py
+++ Myweb/Myapp/views.py
@@ -1,13 +1,14 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.Myapp, name='Myapp'),
-]
-
+from django.shortcuts import render
 from django.http import HttpResponse
-def Myapp(request):
+def student_show(request):
+    x = []
+    for i in range(10):
+        x.append(i)
+    return HttpResponse("<h1>DataFlair Django Tutorials</h1>The Digits are {0}".format(x))
+
+from django.shortcuts import render
+from django.http import HttpResponse
+def index(request):
     x = []
     for i in range(10):
         x.append(i)
